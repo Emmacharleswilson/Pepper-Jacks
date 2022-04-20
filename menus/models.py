776877bb06de00205b98992fabe6cc972e71a28e
Emmacharleswilson <emmacharleswilson@googlemail.com>
2022-04-20 12:07:55 +0000

    Update FoodItem and DrinkItem models so that the admin can select which category they would like to add that specific drink or food to

--- menus/models.py
+++ menus/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+FOOD_MENU_SECTION = ((0, "Breakfast/Brunch"), (1, "Dinner"), (2, "New item"))
+DRINKS_MENU_SECTION = (
+    (0, "Hot Drinks"), (1, "Fruit juices/Soft Drinks"),
+    (2, "Wine/Beer/Cocktails"), (3, "New item"))
+
 # Create your models here.
 
 
@@ -11,8 +16,13 @@ class FoodItem(models.Model):
     dish_name = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200, unique=True)
     price = models.FloatField()
+    dietary = models.CharField(max_length=200)
     allergens = models.CharField(max_length=200, null=True)
-    on_menu = models.BooleanField(default=True)
+    food_menu_section = models.IntegerField(
+        choices=FOOD_MENU_SECTION, default=2)
+    on_menu = models.BooleanField(default=False)
+    updated_on = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         ordering = ['-on_menu']
@@ -29,8 +39,12 @@ class DrinkItem(models.Model):
     drink_name = models.CharField(max_length=200, unique=True)
     description = models.CharField(max_length=200, unique=True)
     price = models.FloatField()
+    dietary = models.CharField(max_length=200)
     allergens = models.CharField(max_length=200, null=True)
+    drinks_menu_section = models.IntegerField(
+        choices=DRINKS_MENU_SECTION, default=3)
     on_menu = models.BooleanField(default=False)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-on_menu']
