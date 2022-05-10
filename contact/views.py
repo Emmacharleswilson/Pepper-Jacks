89758d40b20b8e1a024deb4d2a2f044c4a758a03
Emmacharleswilson <emmacharleswilson@googlemail.com>
2022-05-10 13:44:28 +0000

    Update home page about us text

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
+from django.conf import settings
 
 
 # Create your views here.
@@ -9,24 +10,22 @@ def homepage(request):
     return render(request, "index.html")
 
 
+def send_message(request, contact_form):
+    """function to send email when message is submitted"""
+    customer_fname = contact_form.cleaned_data['first_name']
+    customer_lname = contact_form.cleaned_data['last_name']
+    email_from = contact_form.cleaned_data['email_address']
+    subject = (f'Message from {customer_fname}, {customer_lname}, {email_from}')
+    message = contact_form.cleaned_data['message']
+    recipient_list = [settings.EMAIL_HOST_USER]
+    send_mail(subject, message, email_from, recipient_list)
+
+
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = "Website Inquiry"
-            body = {
-            'first_name': form.cleaned_data['first_name'],
-			'last_name': form.cleaned_data['last_name'],
-			'email': form.cleaned_data['email_address'],
-            'message': form.cleaned_data['message'],
-            }
-            message = "\n".join(body.values())
-
-            try:
-                send_mail(subject, message, 'admin@example.com', ['admin@example.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect("home")
+            send_message(request, form)
 
     form = ContactForm()
-    return render(request, "contact.html", {'form':form})
+    return render(request, "contact.html", {'form': form})
--- pepper_jacks/settings.py
+++ pepper_jacks/settings.py
@@ -64,8 +64,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'EMAIL_HOST_USER'
+EMAIL_HOST_PASSWORD = 'gstvccsbcawhcqrl'
 
 # User settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
@@ -189,8 +189,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Use BOOTSTRAP3 if you are using Bootstrap 3
 BOOTSTRAP4 = {
