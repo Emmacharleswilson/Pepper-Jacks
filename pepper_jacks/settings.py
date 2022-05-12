f13670fc01a36cde19da6899b9063e8c9701b2a8
Emmacharleswilson <emmacharleswilson@googlemail.com>
2022-05-12 10:38:17 +0000

    Redeploy latest version

--- pepper_jacks/settings.py
+++ pepper_jacks/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pepper-jacks.herokuapp.com', 'localhost']
 
@@ -189,7 +189,7 @@ BOOTSTRAP4 = {
 
 # Email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
